ddd6b40eb0e1f964ac942cbb3eaae9daf0894fac
PiTheGuy <the4mooresrock@gmail.com>
2025-01-15 14:01:40 -0600

    Add CLI integration

--- src/pitheguy/sudoku/Main.java
+++ src/pitheguy/sudoku/Main.java
@@ -1,9 +1,25 @@
 package pitheguy.sudoku;
 
+import org.apache.commons.cli.*;
 import pitheguy.sudoku.gui.Sudoku;
 
 public class Main {
-    public static void main(String[] args) {
-        new Sudoku(true);
+    public static void main(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = parser.parse(createOptions(), args);
+        if (commandLine.hasOption("help")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("java -jar Sudoku.jar", createOptions());
+            return;
+        }
+        Sudoku sudoku = new Sudoku(true);
+        if (commandLine.hasOption("puzzle")) sudoku.loadPuzzle(Integer.parseInt(commandLine.getOptionValue("puzzle")));
+    }
+
+    public static Options createOptions() {
+        Options options = new Options();
+        options.addOption("help", false, "Print this message");
+        options.addOption("puzzle", true, "Import a puzzle on startup");
+        return options;
     }
 }
--- src/pitheguy/sudoku/solver/SolverChecker.java
+++ src/pitheguy/sudoku/solver/SolverChecker.java
@@ -1,5 +1,6 @@
 package pitheguy.sudoku.solver;
 
+import org.apache.commons.cli.*;
 import pitheguy.sudoku.gui.Sudoku;
 
 import java.util.*;
@@ -8,33 +9,52 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SolverChecker {
-    public static final int ITERATIONS = 25000;
-    public static final int PROGRESS_UPDATE_INTERVAL = 10000;
-    public static final int MAX_SHOWN_PUZZLES = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = parser.parse(createOptions(), args);
+        int iterations = Integer.parseInt(commandLine.getOptionValue("iterations", "10000"));
+        int progressUpdateInterval = Integer.parseInt(commandLine.getOptionValue("progressUpdateInterval", "10000"));
+        int maxShownPuzzles = Integer.parseInt(commandLine.getOptionValue("maxShownPuzzles", "10"));
+        run(iterations, progressUpdateInterval, maxShownPuzzles);
+    }
+
+    public static Options createOptions() {
+        Options options = new Options();
+        options.addOption("iterations", true, "Number of iterations to run");
+        options.addOption("progressUpdateInterval", true, "Progress update interval");
+        options.addOption("maxShownPuzzles", true, "Maximum number of unsolved puzzles to show on completion");
+        return options;
+    }
+
+    private static void run(int iterations, int progressUpdateInterval, int maxShownPuzzles) {
         List<Integer> unsolved = Collections.synchronizedList(new ArrayList<>());
         AtomicInteger completed = new AtomicInteger(0);
         ThreadLocal<Sudoku> threadLocalSudoku = ThreadLocal.withInitial(() -> new Sudoku(false));
-        IntStream.range(1, ITERATIONS).parallel().forEach(i -> {
+        IntStream.range(1, iterations).parallel().forEach(i -> {
             Sudoku sudoku = threadLocalSudoku.get();
             sudoku.loadPuzzle(i);
             sudoku.solvePuzzle();
             if (!sudoku.isSolved()) unsolved.add(i);
-            if (completed.incrementAndGet() % PROGRESS_UPDATE_INTERVAL == 0) System.out.println("Progress: " + completed.get() + " / " + ITERATIONS);
+            printProgressIfNeeded(iterations, progressUpdateInterval, completed.incrementAndGet());
         });
 
-        int solvedPuzzles = ITERATIONS - unsolved.size();
-        double percent = (double) solvedPuzzles / ITERATIONS * 100;
-        System.out.printf("Solved %d of %d puzzles (%.2f%%)%n", solvedPuzzles, ITERATIONS, percent);
+        int solvedPuzzles = iterations - unsolved.size();
+        double percent = (double) solvedPuzzles / iterations * 100;
+        System.out.printf("Solved %d of %d puzzles (%.2f%%)%n", solvedPuzzles, iterations, percent);
         if (!unsolved.isEmpty()) {
             Collections.sort(unsolved);
             StringBuilder sb = new StringBuilder();
             sb.append("Unsolved puzzles: ");
-            sb.append(unsolved.stream().limit(MAX_SHOWN_PUZZLES).map(String::valueOf).collect(Collectors.joining(", ")));
-            if (unsolved.size() > MAX_SHOWN_PUZZLES) sb.append(", and ").append(unsolved.size() - MAX_SHOWN_PUZZLES).append(" more...");
+            sb.append(unsolved.stream().limit(maxShownPuzzles).map(String::valueOf).collect(Collectors.joining(", ")));
+            if (unsolved.size() > maxShownPuzzles) sb.append(", and ").append(unsolved.size() - maxShownPuzzles).append(" more...");
             System.out.println(sb);
         }
+    }
+
+    private static void printProgressIfNeeded(int iterations, int progressUpdateInterval, int completedPuzzles) {
+        if (progressUpdateInterval == 0) return;
+        if (completedPuzzles % progressUpdateInterval == 0) System.out.println("Progress: " + completedPuzzles + " / " + iterations);
     }
 
 
