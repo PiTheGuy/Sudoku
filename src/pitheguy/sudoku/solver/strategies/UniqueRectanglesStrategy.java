92d106b9455e7476b5d9cc98617048e0e0c4e072
PiTheGuy <the4mooresrock@gmail.com>
2025-01-31 12:55:45 -0600

    Unique Rectangles Type 5

--- src/pitheguy/sudoku/solver/strategies/UniqueRectanglesStrategy.java
+++ src/pitheguy/sudoku/solver/strategies/UniqueRectanglesStrategy.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class UniqueRectanglesStrategy extends SolveStrategy {
@@ -24,6 +25,7 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
         changed |= Type2.solve(sudoku);
         changed |= Type3.solve(sudoku);
         changed |= Type4.solve(sudoku);
+        changed |= Type5.solve(sudoku);
         return changed;
     }
 
@@ -52,12 +54,12 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
                     Square square2 = squares.getFirst() == square1 ? squares.get(1) : squares.get(0);
                     for (int otherIndex = 0; otherIndex < 9; otherIndex++) {
                         if (index == otherIndex) continue;
-                        Rectangle rectangle = new Rectangle(sudoku, square1, square2, otherIndex);
-                        if (rectangle.square3().isSolved() || rectangle.square4().isSolved()) continue;
-                        if (square1.getBox() != square2.getBox() && square1.getBox() != rectangle.square3().getBox())
+                        Rectangle rectangle = new Rectangle(square1, square2, otherIndex);
+                        if (rectangle.square3(sudoku).isSolved() || rectangle.square4(sudoku).isSolved()) continue;
+                        if (square1.getBox() != square2.getBox() && square1.getBox() != rectangle.square3(sudoku).getBox())
                             continue;
-                        if (rectangle.square3().getCandidates().containsAll(floorCandidates) &&
-                            rectangle.square4().getCandidates().containsAll(floorCandidates)) rectangles.add(rectangle);
+                        if (rectangle.square3(sudoku).getCandidates().containsAll(floorCandidates) &&
+                            rectangle.square4(sudoku).getCandidates().containsAll(floorCandidates)) rectangles.add(rectangle);
                     }
                 }
             }
@@ -69,10 +71,10 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
             List<Rectangle> rectangles = findUniqueRectangles(sudoku);
             for (Rectangle rectangle : rectangles) {
                 DigitCandidates candidates = rectangle.square1().getCandidates();
-                if (!rectangle.square3().getCandidates().equals(candidates)) continue;
-                if (rectangle.square4().isSolved()) continue;
-                if (!rectangle.square4().getCandidates().containsAll(candidates)) continue;
-                if (rectangle.square4().getCandidates().removeAll(candidates)) return true;
+                if (!rectangle.square3(sudoku).getCandidates().equals(candidates)) continue;
+                if (rectangle.square4(sudoku).isSolved()) continue;
+                if (!rectangle.square4(sudoku).getCandidates().containsAll(candidates)) continue;
+                if (rectangle.square4(sudoku).getCandidates().removeAll(candidates)) return true;
             }
             return false;
         }
@@ -82,17 +84,17 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
         private static boolean solve(Sudoku sudoku) {
             List<Rectangle> rectangles = findUniqueRectangles(sudoku);
             for (Rectangle rectangle : rectangles) {
-                if (!rectangle.square3().getCandidates().equals(rectangle.square4().getCandidates())) continue;
-                if (rectangle.square3().getCandidates().count() != 3) continue;
-                DigitCandidates candidates = rectangle.square3().getCandidates().copy();
+                if (!rectangle.square3(sudoku).getCandidates().equals(rectangle.square4(sudoku).getCandidates())) continue;
+                if (rectangle.square3(sudoku).getCandidates().count() != 3) continue;
+                DigitCandidates candidates = rectangle.square3(sudoku).getCandidates().copy();
                 candidates.removeAll(rectangle.floorCandidates());
                 int extraDigit = candidates.getFirst();
-                List<Square> surroundingUnit = rectangle.isRowWise() ? rectangle.square3().getSurroundingRow() : rectangle.square3().getSurroundingColumn();
+                List<Square> surroundingUnit = rectangle.isRowWise() ? rectangle.square3(sudoku).getSurroundingRow() : rectangle.square3(sudoku).getSurroundingColumn();
                 Set<Square> affectedSquares = new HashSet<>(surroundingUnit);
-                if (rectangle.square3().getBox() == rectangle.square4().getBox())
-                    affectedSquares.addAll(rectangle.square3().getSurroundingBox());
-                affectedSquares.remove(rectangle.square3());
-                affectedSquares.remove(rectangle.square4());
+                if (rectangle.square3(sudoku).getBox() == rectangle.square4(sudoku).getBox())
+                    affectedSquares.addAll(rectangle.square3(sudoku).getSurroundingBox());
+                affectedSquares.remove(rectangle.square3(sudoku));
+                affectedSquares.remove(rectangle.square4(sudoku));
                 affectedSquares.removeIf(Square::isSolved);
                 boolean changed = false;
                 for (Square square : affectedSquares) changed |= square.getCandidates().remove(extraDigit);
@@ -106,13 +108,13 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
         private static boolean solve(Sudoku sudoku) {
             List<Rectangle> rectangles = findUniqueRectangles(sudoku);
             for (Rectangle rectangle : rectangles) {
-                DigitCandidates candidates = rectangle.square3().getCandidates().or(rectangle.square4().getCandidates());
+                DigitCandidates candidates = rectangle.square3(sudoku).getCandidates().or(rectangle.square4(sudoku).getCandidates());
                 if (candidates.count() != 4) continue;
                 candidates.removeAll(rectangle.floorCandidates());
-                List<Square> surroundingUnit = rectangle.isRowWise() ? rectangle.square3().getSurroundingRow() : rectangle.square3().getSurroundingColumn();
+                List<Square> surroundingUnit = rectangle.isRowWise() ? rectangle.square3(sudoku).getSurroundingRow() : rectangle.square3(sudoku).getSurroundingColumn();
                 Square otherSquare = null;
                 for (Square square : surroundingUnit) {
-                    if (square == rectangle.square3() || square == rectangle.square4()) continue;
+                    if (square == rectangle.square3(sudoku) || square == rectangle.square4(sudoku)) continue;
                     if (square.isSolved()) continue;
                     if (square.getCandidates().equals(candidates)) {
                         otherSquare = square;
@@ -123,7 +125,7 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
                 boolean changed = false;
                 for (Square square : surroundingUnit) {
                     if (square.isSolved()) continue;
-                    if (square == rectangle.square3() || square == rectangle.square4() || square == otherSquare)
+                    if (square == rectangle.square3(sudoku) || square == rectangle.square4(sudoku) || square == otherSquare)
                         continue;
                     changed |= square.getCandidates().removeAll(candidates);
                 }
@@ -139,21 +141,21 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
             for (Rectangle rectangle : rectangles) {
                 List<Integer> candidates = rectangle.floorCandidates().getAllCandidates();
                 for (int digit : candidates) {
-                    List<Square> surroundingUnit = rectangle.isRowWise() ? rectangle.square3().getSurroundingRow() : rectangle.square3().getSurroundingColumn();
+                    List<Square> surroundingUnit = rectangle.isRowWise() ? rectangle.square3(sudoku).getSurroundingRow() : rectangle.square3(sudoku).getSurroundingColumn();
                     boolean hasNoDigit = true;
                     for (Square square : surroundingUnit) {
                         if (square.isSolved()) continue;
-                        if (square == rectangle.square3() || square == rectangle.square4()) continue;
+                        if (square == rectangle.square3(sudoku) || square == rectangle.square4(sudoku)) continue;
                         if (square.getCandidates().contains(digit)) {
                             hasNoDigit = false;
                             break;
                         }
                     }
-                    if (rectangle.square3().getBox() == rectangle.square4().getBox()) {
+                    if (rectangle.square3(sudoku).getBox() == rectangle.square4(sudoku).getBox()) {
                         boolean hasNoDigitInBox = true;
-                        for (Square square : rectangle.square3().getSurroundingBox()) {
+                        for (Square square : rectangle.square3(sudoku).getSurroundingBox()) {
                             if (square.isSolved()) continue;
-                            if (square == rectangle.square3() || square == rectangle.square4()) continue;
+                            if (square == rectangle.square3(sudoku) || square == rectangle.square4(sudoku)) continue;
                             if (square.getCandidates().contains(digit)) {
                                 hasNoDigitInBox = false;
                                 break;
@@ -164,8 +166,8 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
                     if (hasNoDigit) {
                         int otherDigit = candidates.getFirst() == digit ? candidates.getLast() : candidates.getFirst();
                         boolean changed = false;
-                        changed |= rectangle.square3().getCandidates().remove(otherDigit);
-                        changed |= rectangle.square4().getCandidates().remove(otherDigit);
+                        changed |= rectangle.square3(sudoku).getCandidates().remove(otherDigit);
+                        changed |= rectangle.square4(sudoku).getCandidates().remove(otherDigit);
                         if (changed) return true;
                     }
                 }
@@ -174,16 +176,85 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
         }
     }
 
-    private record Rectangle(Sudoku sudoku, Square square1, Square square2, int otherIndex) {
+    private static class Type5 {
+        private static boolean solve(Sudoku sudoku) {
+            List<Rectangle> rectangles = new ArrayList<>();
+            for (int row1 = 0; row1 < 9; row1++) {
+                for (int row2 = row1 + 1; row2 < 9; row2++) {
+                    for (int col1 = 0; col1 < 9; col1++) {
+                        for (int col2 = col1 + 1; col2 < 9; col2++) {
+                            Square square1 = sudoku.getSquare(row1, col1);
+                            Square square2 = sudoku.getSquare(row1, col2);
+                            Rectangle rectangle = new Rectangle(square1, square2, row2);
+                            if (rectangle.getCorners(sudoku).stream().anyMatch(Square::isSolved)) continue;
+                            if (square1.getBox() != square2.getBox() && square1.getBox() != rectangle.square3(sudoku).getBox()) continue;
+                            DigitCandidates sharedCandidates = rectangle.getCorners(sudoku).stream().map(Square::getCandidates).reduce(new DigitCandidates(), DigitCandidates::and);
+                            if (sharedCandidates.count() != 2) continue;
+                            if (square1.getCandidates().equals(sharedCandidates) && rectangle.square4(sudoku).getCandidates().equals(sharedCandidates))
+                                rectangles.add(rectangle);
+                            else if (square2.getCandidates().equals(sharedCandidates) && rectangle.square3(sudoku).getCandidates().equals(sharedCandidates))
+                                rectangles.add(rectangle);
+                        }
+                    }
+                }
+            }
+            for (Rectangle rectangle : rectangles) {
+                boolean isLeftDiagonal = rectangle.square1().getCandidates().count() == 2;
+                Square floor1;
+                Square floor2;
+                if (isLeftDiagonal) {
+                    floor1 = rectangle.square1();
+                    floor2 = rectangle.square4(sudoku);
+                } else {
+                    floor1 = rectangle.square2();
+                    floor2 = rectangle.square3(sudoku);
+                }
+                boolean changed = false;
+                changed |= processSquare(sudoku, rectangle, floor1);
+                changed |= processSquare(sudoku, rectangle, floor2);
+                if (changed) return true;
+            }
+            return false;
+        }
+
+        private static boolean processSquare(Sudoku sudoku, Rectangle rectangle, Square square) {
+            Set<Square> squaresToCheck = new HashSet<>();
+            squaresToCheck.addAll(square.getSurroundingRow());
+            squaresToCheck.addAll(square.getSurroundingColumn());
+            rectangle.getCorners(sudoku).forEach(squaresToCheck::remove);
+            squaresToCheck.removeIf(Square::isSolved);
+            Optional<Integer> strongLinkDigit = Optional.empty();
+            for (int digit : square.getCandidates().getAllCandidates()) {
+                boolean containsDigit = false;
+                for (Square current : squaresToCheck) {
+                    if (current.getCandidates().contains(digit)) {
+                        containsDigit = true;
+                        break;
+                    }
+                }
+                if (!containsDigit) {
+                    if (strongLinkDigit.isPresent()) strongLinkDigit = Optional.empty();
+                    else strongLinkDigit = Optional.of(digit);
+                }
+            }
+            if (strongLinkDigit.isEmpty()) return false;
+            DigitCandidates copy = square.getCandidates().copy();
+            copy.remove(strongLinkDigit.get());
+            int targetDigit = copy.getFirst();
+            return square.getCandidates().remove(targetDigit);
+        }
+    }
+
+    private record Rectangle(Square square1, Square square2, int otherIndex) {
         public boolean isRowWise() {
             return square1.getRow() == square2.getRow();
         }
 
-        public Square square3() {
+        public Square square3(Sudoku sudoku) {
             return isRowWise() ? sudoku.getSquare(otherIndex, square1.getCol()) : sudoku.getSquare(square1.getRow(), otherIndex);
         }
 
-        public Square square4() {
+        public Square square4(Sudoku sudoku) {
             return isRowWise() ? sudoku.getSquare(otherIndex, square2.getCol()) : sudoku.getSquare(square2.getRow(), otherIndex);
         }
 
@@ -191,9 +262,16 @@ public class UniqueRectanglesStrategy extends SolveStrategy {
             return square1.getCandidates();
         }
 
+        public List<Square> getCorners(Sudoku sudoku) {
+            return List.of(square1, square2, square3(sudoku), square4(sudoku));
+        }
+
         @Override
         public String toString() {
-            return "[" + square1 + ", " + square2 + ", " + square3() + ", " + square4() + "]";
+            if (isRowWise())
+                return "[R" + square1.getRow() + otherIndex + "C" + square1.getCol() + square2.getCol() + "]";
+            else
+                return "[" + square1.getRow() + square2.getRow() + "C" + square1.getCol() + otherIndex + "]";
         }
     }
 }
